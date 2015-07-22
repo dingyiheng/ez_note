8a6d46a31ef1f7608f70e8b569b7577edb773257
Yiheng Ding <Ding@Yihengs-MacBook.local>
2015-07-22 16:03:53 -0500

    Audio view add father class ezview and protocol

---
+++ ez_note/EZnoteProtocol.h
@@ -0,0 +1,15 @@
+//
+//  EZnoteProtocol.h
+//  try_20150719_audioView
+//
+//  Created by Yiheng Ding on 7/22/15.
+//  Copyright (c) 2015 Yiheng Ding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol EZnoteProtocol <NSObject>
+
+- (id) getOutput;
+
+@end
---
+++ ez_note/Ezview.m
@@ -0,0 +1,17 @@
+//
+//  Ezview.m
+//  try_20150719_audioView
+//
+//  Created by Yiheng Ding on 7/22/15.
+//  Copyright (c) 2015 Yiheng Ding. All rights reserved.
+//
+
+#import "Ezview.h"
+
+@implementation Ezview
+
+-(id)getOutput{
+    return 0;
+}
+
+@end
