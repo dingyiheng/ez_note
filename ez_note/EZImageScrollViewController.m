2a50d7ef564adc570fa5cbccfd826a589042edd4
Ren Wan <wanren13@hotmail.com>
2015-07-28 00:06:25 -0500

    Refactored EZImageScrollViewController Class Added updateImageView method to handle rotation

--- ez_note/EZImageScrollViewController.m
+++ ez_note/EZImageScrollViewController.m
@@ -39,73 +39,28 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    
-    // Create a scroll view
-    
-    sView = [[UIScrollView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
-    
-    sView.delegate = self;
-    sView.bouncesZoom = YES;
-    
-    sView.backgroundColor = [UIColor blackColor];
-    
-  
     img_width = self.img.size.width;
     img_height = self.img.size.height;
     img_ratio = img_height / img_width;
     
-    imgview_start_x = 0;
-    imgview_start_y = 0;
-    imgview_width = screen_width;
-    imgview_height = screen_height;
     
-    zoomscale = 1;
+    // Create a scroll view
     
-    // over size
-    if (img_height > screen_height || img_width > screen_width){
-        // thin tall
-        if (img_width < screen_width) {
-            imgview_start_x = (screen_width - img_width) / 2;
-//            content_offset_x = (screen_width - img_width) / 2;
-            imgview_width = img_width;
-            imgview_height = img_height;
-        }
-        else {
-            zoomscale = img_width / screen_width;
-            imgview_height = img_height / zoomscale;
-            
-            // wide
-            if (imgview_height < screen_height) {
-                imgview_start_y = (screen_height - imgview_height) / 2;
-//                content_offset_y = (screen_height - imgview_height) / 2;
-            }
-        }
-    }
-    else {
-        imgview_start_x = (screen_width - img_width) / 2;
-        imgview_start_y = (screen_height -img_height) / 2;
-        imgview_width = img_width;
-        imgview_height = img_height;
-    }
-        
-    CGRect frame = CGRectMake(imgview_start_x, imgview_start_y, imgview_width, imgview_height);
-    imageView = [[UIImageView alloc] initWithFrame:frame];
+    sView = [[UIScrollView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
+    imageView = [UIImageView new];
     imageView.image = self.img;
+    
+    sView.delegate = self;
+    sView.bouncesZoom = YES;
+    sView.backgroundColor = [UIColor blackColor];
+    
+    [self updateImageView];
     
     [sView addSubview:imageView];
     
-//    [sView sizeToFit];
-    sView.contentSize = imageView.bounds.size;
-    
-    // Minimum and maximum zoom scales
-    
-    // TODO
-    sView.minimumZoomScale = 1.0;
-    sView.maximumZoomScale = zoomscale * 2.0;
-    
     self.view = sView;
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateImageViewFrame) name:UIDeviceOrientationDidChangeNotification object: nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateImageView) name:UIDeviceOrientationDidChangeNotification object: nil];
 //    printf("sView Bounds originX:%f originY:%f width:%f  height:%f\n", sView.bounds.origin.x, sView.bounds.origin.y, sView.bounds.size.width, sView.bounds.size.height);
 //    printf("sView ContentOffset x:%f  y:%f  ContentSize width %f  height:%f\n", sView.contentOffset.x, sView.contentOffset.y, sView.contentSize.width, sView.contentSize.height);
 //    printf("sView Frame originX:%f originY:%f width %f  height:%f\n", sView.frame.origin.x, sView.frame.origin.y, sView.frame.size.width, sView.frame.size.height);
@@ -132,16 +87,56 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void) updateImageView {
+    imgview_start_x = 0;
+    imgview_start_y = 0;
+    imgview_width = screen_width;
+    imgview_height = screen_height;
+    
+    zoomscale = 1;
+    
+    // over size
+    if (img_height > screen_height || img_width > screen_width){
+        // thin tall
+        if (img_width < screen_width) {
+            imgview_start_x = (screen_width - img_width) / 2;
+            //            content_offset_x = (screen_width - img_width) / 2;
+            imgview_width = img_width;
+            imgview_height = img_height;
+        }
+        else {
+            zoomscale = img_width / screen_width;
+            imgview_height = img_height / zoomscale;
+            
+            // wide
+            if (imgview_height < screen_height) {
+                imgview_start_y = (screen_height - imgview_height) / 2;
+                //                content_offset_y = (screen_height - imgview_height) / 2;
+            }
+        }
+    }
+    else {
+        imgview_start_x = (screen_width - img_width) / 2;
+        imgview_start_y = (screen_height -img_height) / 2;
+        imgview_width = img_width;
+        imgview_height = img_height;
+    }
+    
+    CGRect frame = CGRectMake(imgview_start_x, imgview_start_y, imgview_width, imgview_height);
+    imageView.frame = frame;
+    
+//    imageView.image = self.img;
+    
+    //    [sView sizeToFit];
+    sView.contentSize = imageView.bounds.size;
+    
+    // Minimum and maximum zoom scales
+    
+    sView.minimumZoomScale = 1.0;
+    sView.maximumZoomScale = zoomscale * 2.0;
+
 }
-*/
-
 
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
@@ -173,6 +168,9 @@
 
 - (void)updateImageViewFrame {
     CGRect frame = imageView.frame;
+    
+    
+    NSLog (@"Screen width: %f  Screen height:  %f", screen_width, screen_height);
     
     if (frame.size.width < screen_width) {
         frame.origin.x = (screen_width - frame.size.width) / 2;
